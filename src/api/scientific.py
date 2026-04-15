7b0026d357e83e50340ec2a839265bbde348ac4c
Paras-test <itzobitotobi@gmail.com>
2026-04-15 14:00:17 +0530

    Add scientific operations and unit tests

--- src/api/scientific.py
+++ src/api/scientific.py
@@ -1,13 +1,16 @@
-from math import sin, cos, tan, log
+import math
 
-def sine(angle):
-    return sin(angle)
+def sine(x):
+    return math.sin(math.radians(x))
 
-def cosine(angle):
-    return cos(angle)
+def cosine(x):
+    return math.cos(math.radians(x))
 
-def tangent(angle):
-    return tan(angle)
+def tangent(x):
+    return math.tan(math.radians(x))
 
-def logarithm(value):
-    return log(value)
+def log(x):
+    if x > 0:
+        return math.log(x)
+    else:
+        raise ValueError('Logarithm is not defined for non-positive numbers')
