6bfab417e3e46f51be16084759dc90bc4ca9fd62
Paras-test <itzobitotobi@gmail.com>
2026-04-15 13:58:40 +0530

    Add advanced scientific operations module

---
+++ src/api/scientific.py
@@ -0,0 +1,13 @@
+from math import sin, cos, tan, log
+
+def sine(angle):
+    return sin(angle)
+
+def cosine(angle):
+    return cos(angle)
+
+def tangent(angle):
+    return tan(angle)
+
+def logarithm(value):
+    return log(value)
