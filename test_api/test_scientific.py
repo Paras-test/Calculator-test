311fa1e702721340619048ca54ea90b2715dfa3b
Paras-test <itzobitotobi@gmail.com>
2026-04-15 14:01:58 +0530

    Add scientific operations module and unit tests

--- test_api/test_scientific.py
+++ test_api/test_scientific.py
@@ -1,27 +1,26 @@
 import unittest
-from src.api.scientific import sine, cosine, tangent, log
+from src.api.scientific import sin, cos, tan, log
 
 class TestScientificOperations(unittest.TestCase):
-    def test_sine(self):
-        self.assertAlmostEqual(sine(0), 0)
-        self.assertAlmostEqual(sine(90), 1)
-        self.assertAlmostEqual(sine(180), 0)
-        self.assertAlmostEqual(sine(270), -1)
+    def test_sin(self):
+        self.assertAlmostEqual(sin(0), 0)
+        self.assertAlmostEqual(sin(30), math.sin(math.radians(30)))
+        self.assertAlmostEqual(sin(60), math.sin(math.radians(60)))
+        self.assertAlmostEqual(sin(90), 1)
 
-    def test_cosine(self):
-        self.assertAlmostEqual(cosine(0), 1)
-        self.assertAlmostEqual(cosine(90), 0)
-        self.assertAlmostEqual(cosine(180), -1)
-        self.assertAlmostEqual(cosine(270), 0)
+    def test_cos(self):
+        self.assertAlmostEqual(cos(0), 1)
+        self.assertAlmostEqual(cos(30), math.cos(math.radians(30)))
+        self.assertAlmostEqual(cos(60), math.cos(math.radians(60)))
+        self.assertAlmostEqual(cos(90), 0)
 
-    def test_tangent(self):
-        self.assertAlmostEqual(tangent(0), 0)
-        self.assertAlmostEqual(tangent(45), 1)
-        self.assertAlmostEqual(tangent(90), math.inf)
-        self.assertAlmostEqual(tangent(180), 0)
-        self.assertAlmostEqual(tangent(270), -math.inf)
+    def test_tan(self):
+        self.assertAlmostEqual(tan(0), 0)
+        self.assertAlmostEqual(tan(30), math.tan(math.radians(30)))
+        self.assertAlmostEqual(tan(60), math.tan(math.radians(60)))
+        self.assertAlmostEqual(tan(90), float('inf'))
 
     def test_log(self):
         self.assertAlmostEqual(log(1), 0)
-        self.assertAlmostEqual(log(10), 1)
-        self.assertAlmostEqual(log(100), 2)
+        self.assertAlmostEqual(log(2), math.log(2))
+        self.assertAlmostEqual(log(10), math.log(10))
